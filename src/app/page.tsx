3eb9ce815b65ba2a686eb40bc66c1f4c466e7e03
osamaiqbal <osamaiqbal.alphasquad@gmail.com>
2025-02-19 13:24:09 +0500

    refactor(layout): restructure main component for improved readability and layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,23 @@ export default function Home() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
-    <div className="flex">
+    <main
+      className={`bg-primary flex  flex-row text-white transition-all duration-300  w-full
+      `}
+    >
       <Sidebar
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <main
-        className={`bg-primary flex  flex-col text-white transition-all duration-300 ${
-          isSidebarOpen ? 'w-[calc(100%-15rem)]' : 'w-full'
-        }`}
-      >
+      <div className="flex flex-col w-full bg-primary">
         <Navbar />
-        <div className='flex flex-1'>
+        <div className="flex flex-1">
           <PlayerMenu />
           <div className="flex w-full">
             <PlayerInfoContainer />
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
